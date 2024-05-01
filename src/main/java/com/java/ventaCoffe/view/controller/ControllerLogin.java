dbcf110fde8c99e727d5d1f52f9d18f16599c5d1
AbelBusi <cesarabelbustamante@gmail.com>
2024-04-30 23:07:49 -0500

    Implementacion del login del usuario

--- src/main/java/com/java/ventaCoffe/view/controller/ControllerLogin.java
+++ src/main/java/com/java/ventaCoffe/view/controller/ControllerLogin.java
@@ -1,6 +1,69 @@
 package com.java.ventaCoffe.view.controller;
 
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import org.springframework.stereotype.Component;
 
-public class ControllerLogin {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class ControllerLogin implements Initializable {
+
+    @FXML
+    private Button BCrearCuenta;
+
+    @FXML
+    private Button BIngresar;
+
+    @FXML
+    private Button BRegistrarse;
+
+    @FXML
+    private Label LCrearCuenta;
+
+    @FXML
+    private Label LIngresarUsuario;
+
+    @FXML
+    private Label LRegistrarse;
+
+    @FXML
+    private AnchorPane apEntrarRegistrarse;
+
+    @FXML
+    private AnchorPane apIngresar;
+
+    @FXML
+    private AnchorPane apRegistrarse;
+
+    @FXML
+    private ComboBox<?> comboPregunta;
+
+    @FXML
+    private Hyperlink hyClave;
+
+    @FXML
+    private PasswordField txtClave;
+
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private PasswordField txtRegistrarClave;
+
+    @FXML
+    private TextField txtRegistrarseCorreo;
+
+    @FXML
+    private TextField txtRespuesta;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
